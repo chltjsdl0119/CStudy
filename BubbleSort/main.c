8e18b006dcd849cef8d1e815feace669dbc76fcc
SeungwonChoi <bestwondev@gmail.com>
2024-02-13 12:37:44 +0900

    Sort Algorithm
    Bubble Sort

---
+++ BubbleSort/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    int temp;
+    int array[10] = {1, 10, 5, 8, 7, 5, 4, 3, 2, 9};
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 9 - i; ++j) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", array[i]);
+    }
+
+    return;
+}
